1682c7545cfc216819228d0e1462c9f6796dfae3
Dtrugk <kiendt273@gmail.com>
2024-09-07 00:37:15 +0700

    Changes to image file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['./assets/Pic3.png'], // Correct path, but not recommended for images
+    },
+  },
 })
